d10bbfe48d87051acf7de778200a5c78a1c849c1
Madmaxcoder2612 <72246796+Madmaxcoder2612@users.noreply.github.com>
2020-10-04 13:15:59 +0530

    Added the try and except feature .
    This feature avoids the error of traceback.

--- PR3.py
+++ PR3.py
@@ -1,8 +1,10 @@
 print("The salary and expenses Calculator:")
-salary = input ("Insert the value of your salary:")
-s = float(salary)
-expenses = input ("Insert the value of your expenses:")
-e=float(expenses)
-percentage = (e*100)/s
-print("Your spend ",percentage ,"%", "of your salary in your expenses.")
-print("Enter A valid Number !")
+try :
+    salary = input ("Insert the value of your salary:")
+    s = float(salary)
+    expenses = input ("Insert the value of your expenses:")
+    e=float(expenses)
+    percentage = (e*100)/s
+    print("Your spend ",percentage ,"%", "of your salary in your expenses.")
+except :
+    print("Enter A valid Number !")
